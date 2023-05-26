9498ceeb88a072652a604260598c512c1c354954
悠人 菅原 <220369@jc-21.jp>
2023-05-26 11:45:30 +0900

    //要素 Playerの移動処理の記述を進行中 PlayerクラスにCalcurateVector()を追加
    //感想
    いい感じに頭使えばできそう

--- Player.cpp
+++ Player.cpp
@@ -2,6 +2,8 @@
 #include "Engine/Model.h"
 #include "Engine/Input.h"
 
+#include "Engine/Camera.h"
+
 //コンストラクタ
 Player::Player(GameObject* parent)
 	: GameObject(parent, "Player")
@@ -13,6 +15,14 @@ void Player::Initialize()
 {
 	hModel_ = Model::Load("defaultCharacter.fbx");
 	assert(hModel_ >= 0);
+
+	//仮カメラセット
+	camPosition_ = { 0.0f,10.0f,10.0f };
+	camTarget_ = { 00.0f,0.0f,0.0f };
+
+	Camera::SetPosition(camPosition_);
+	Camera::SetTarget(camTarget_);
+
 }
 
 //更新
@@ -24,12 +34,16 @@ void Player::Update()
 	//移動速度
 	float speed = 0.1f;
 
+
 	//空方向ベクトルX
 	XMVECTOR vMoveZ = { 0.0f,0.0f,speed,0.0f };
 	XMVECTOR vMoveX = { speed,0.0f,0.0f,0.0f };
 
+	//カメラのベクトルを取得
+	XMVECTOR vCam = CalucurateVector(camPosition_, camTarget_);
+
 	//画面前方に移動
-	if (Input::IsKey(DIK_W)) { vPos += vMoveZ; }
+	if (Input::IsKey(DIK_W)) { vPos += vMoveZ + XMVector3Normalize(vCam); }
 	//画面右側に移動
 	if (Input::IsKey(DIK_A)) { vPos -= vMoveX; }
 	//画面後方に移動
@@ -53,4 +67,16 @@ void Player::Draw()
 //開放
 void Player::Release()
 {
+}
+
+XMVECTOR Player::CalucurateVector(XMVECTOR _vecA, XMVECTOR _vecB)
+{
+	return XMVECTOR( _vecB - _vecA);
+}
+
+XMVECTOR Player::CalucurateVector(XMFLOAT3 _posA, XMFLOAT3 _posB)
+{
+	_posA.y = 0;
+	_posB.y = 0;
+	return XMVECTOR(XMLoadFloat3(&_posB) - XMLoadFloat3(&_posA));
 }
